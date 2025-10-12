242bafe1e62840247d79ebca85be65140b41b68f
Matt Martini <matt.martini@gmail.com>
2025-10-12 10:00:45 -0400

    feat(bin): Add deployment of executable

---
+++ lib/App/ActiveIPs/Syntax.pm
@@ -0,0 +1,145 @@
+package MERM::Base::Syntax;
+
+use 5.018;
+
+use utf8;
+use strict;
+use warnings;
+use autodie;
+use open qw(:std :utf8);
+
+use Import::Into;
+use Module::Runtime;
+
+use version; our $VERSION = version->declare("v1.0.11");
+
+sub importables {
+    my ($class) = @_;
+    return (
+             [ 'feature', ':5.18' ], 'utf8',
+             'strict',               'warnings',
+             'autodie',              [ 'open', ':std', ':utf8' ],
+             'version',              'Readonly',
+             'Carp',                 [ 'English', '-no_match_vars' ]
+           );
+}
+
+sub import {
+    my (@args) = @_;
+    my $class  = shift @args;
+    my $caller = caller;
+
+    foreach my $import_proto ( $class->importables ) {
+        my $module;
+        ( $module, @args )
+            = ( ref($import_proto) || '' ) eq 'ARRAY'
+            ? @$import_proto
+            : ( $import_proto, () );
+        Module::Runtime::use_module($module)->import::into( $caller, @args );
+    }
+    return;
+}
+
+1;    # End of MERM::Base::Syntax
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MERM::Base::Syntax - Provide consistent feature setup.
+
+=head1 VERSION
+
+Version v.1.0.11
+
+=head1 SYNOPSIS
+
+Provide consistent feature setup.  Put all of the "use" setup cmds in one place.
+Then import them into other modules.
+
+Use this in other modules:
+
+    package MERM::Base::Example;
+
+    use MERM::Base::Syntax;
+
+    # Rest of Code...
+
+This is equivalent to:
+
+    package MERM::Base::Example;
+
+    use feature :5.18;
+    use utf8;
+    use strict;
+    use warnings;
+    use autodie;
+    use open qw(:std :utf8);
+    use version;
+    use Readonly;
+    use Carp;
+    use English qw( -no_match_vars );
+
+    # Rest of Code...
+
+=head1 SUBROUTINES/METHODS
+
+=head2 importables
+
+Define the items to be imported.
+
+=head2 import
+
+Do the import.
+
+=head1 AUTHOR
+
+Matt Martini, C<< <matt at imaginarywave.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-merm-base at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=MERM-Base>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MERM::Base::Syntax
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=MERM-Base>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/MERM-Base>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/MERM-Base>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright © 2024 by Matt Martini.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+=cut
+
+__END__
+
---
+++ lib/App/Base.pm
@@ -0,0 +1,155 @@
+package MERM::Base;
+
+use 5.018;
+use strict;
+use warnings;
+use Carp;
+use lib 'lib';
+
+use version; our $VERSION = version->declare("v1.0.11");
+
+use Exporter   qw( );
+use List::Util qw( uniq );
+
+our @EXPORT      = ();
+our @EXPORT_OK   = ();
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );    # Optional.
+
+sub import {
+    my $class = shift;
+    my (@packages) = @_;
+
+    my ( @pkgs, @rest );
+    for (@packages) {
+        if (/^::/) {
+            push @pkgs, __PACKAGE__ . $_;
+        }
+        else {
+            push @rest, $_;
+        }
+    }
+
+    for my $pkg (@pkgs) {
+        my $mod = ( $pkg =~ s{::}{/}gr ) . ".pm";
+        require $mod;
+
+        my $exports = do { no strict "refs"; \@{ $pkg . "::EXPORT_OK" } };
+        $pkg->import(@$exports);
+        @EXPORT    = uniq @EXPORT,    @$exports;
+        @EXPORT_OK = uniq @EXPORT_OK, @$exports;
+    }
+
+    @_ = ( $class, @rest );
+    goto &Exporter::import;
+}
+
+1;    # End of MERM::Base
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MERM::Base - Base modules for Perl Development
+
+
+=head1 VERSION
+
+Version v.1.0.11
+
+=head1 SYNOPSIS
+
+MERM::Base provides a loader for sub-modules where a leading :: denotes a package to load.
+
+    use MERM::Base qw( ::OS ::Utils );
+
+This is equivalent to:
+
+    user MERM::Base::OS    qw(:all);
+    user MERM::Base::Utils qw(:all);
+
+
+
+=head1 SEE ALSO
+
+L<MERM::Base::Disks>,
+L<MERM::Base::Syntax>,
+L<MERM::Base::Utils>,
+L<MERM::Base::OS>,
+L<MERM::Base::Backup>
+
+=head1 DESCRIPTION
+
+=for author to fill in:
+    Write a full description of the module and its features here.
+    Use subsections (=head2, =head3) as appropriate.
+
+
+
+=back
+
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+MERM::Base requires no configuration files or environment variables.
+
+
+=head1 DEPENDENCIES
+
+None.
+
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-merm-base@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+
+=head1 AUTHOR
+
+Matt Martini  C<< <matt@imaginarywave.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+This software is Copyright © 2024 by Matt Martini.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
+
+__END__
